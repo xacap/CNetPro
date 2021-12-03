3357b8d4899ee384aede407f47b140b946623d58
Oleksii.Iatsuta <xacap.seo@gmail.com>
2021-12-03 23:48:56 +0200

    task 3 4

--- ClassWork_02/ClassWork_02/Program.cs
+++ ClassWork_02/ClassWork_02/Program.cs
@@ -12,38 +12,67 @@ namespace ClassWork_02
 
             //Console.WriteLine(CompareAndCalculate(A, B));
             Console.WriteLine(OrderingNumbers(A, B, C));
-
+            //Console.WriteLine(QuadraticEquation(A, B, C));
+            //SwitchExample();
         }
-        static (int x, int y, int z) OrderingNumbers(int a, int b, int c)
-        {
-            int sumAB = a + b;
-            int sumBC = b + c;
-            int sumAC = a + c;
-            int x, y, z;
 
-            if (sumAB < sumBC)
+        static void SwitchExample()
+        {
+            Console.WriteLine("Enter 1 for task 1");
+            Console.WriteLine("Enter 2 for task 2");
+            Console.WriteLine("Enter 3 for task 3");
+
+            int choise = Convert.ToInt32(Console.ReadLine());
+
+            switch (choise)
             {
-                if (a < b)
-                {
-                    x = a; y = b; z = c;
-                }
-                else
-                {
-                    x = b; y = a; z = c;
-                }
+                case 1:
+                    Console.WriteLine(1);
+                    break;
+                case 2:
+                    Console.WriteLine(2);
+                    break;
+                case 3:
+                    Console.WriteLine(3);
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
             }
-            else
+        }
+
+        static (double x1, double x2) QuadraticEquation(int a, int b, int c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            double x1 = (-discriminant - Math.Sqrt(discriminant)) / 2 * a;
+            double x2 = (-discriminant + Math.Sqrt(discriminant)) / 2 * a;
+
+            return (x1,x2);
+        }
+        static (int a, int b, int c) OrderingNumbers(int a, int b, int c)
+        {
+            int tmp = a;
+
+            if (a > b)
             {
-                if (b < c)
-                {
-                    x = b; y = c; z = a;
-                }
-                else
-                {
-                    x = c; y = b; z = a;
-                }
+                a = b;
+                b = tmp;
+                tmp = a;
             }
-            return (x, y , z);
+            if (a > c)
+            {
+                a = c; 
+                c = tmp;
+                tmp = b;
+            }
+            if (b > c)
+            {
+                b = c;
+                c = tmp;
+            }
+
+            return (a, b, c);
         }
 
         static string CoordinateQuarter(int x, int y)
